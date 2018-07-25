664f19015fd7a9fb4195192ba57286a35bc96b12
Benjamin Florin <benjamin.florin@gmail.com>
2018-07-25 11:33:28 -0400

    Add shim for String.includes(). Fixes #14

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,6 +3,23 @@ import * as ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+/* IE Shims */
+// String.includes()
+if (!String.prototype.includes) {
+    String.prototype.includes = function(search, start) {
+        'use strict';
+        if (typeof start !== 'number') {
+            start = 0;
+        }
+
+        if (start + search.length > this.length) {
+            return false;
+        } else {
+            return this.indexOf(search, start) !== -1;
+        }
+    };
+}
+
 ReactDOM.render(
   <App />,
   document.getElementById('root') as HTMLElement
