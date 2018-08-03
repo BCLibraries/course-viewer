b80a56dc15b9626dccae2f1096cb912accf9c305
Benjamin Florin <benjamin.florin@gmail.com>
2018-08-03 15:26:18 -0400

    Add LDAP search capabilities

---
+++ server/src/Logger.ts
@@ -0,0 +1,15 @@
+import bunyan from "bunyan";
+import path from "path";
+
+const logger = bunyan.createLogger({
+    name: 'main-logger',
+    streams: [{
+        type: 'rotating-file',
+        level: 'info',
+        path: path.join(__dirname, '..', 'log/reserves.log'),
+        period: '1d',   // daily rotation
+        count: 180        // keep 3 back copies
+    }]
+});
+
+export default logger;
--- server/src/auth/LDAP.ts
+++ server/src/auth/LDAP.ts
@@ -1,23 +1,17 @@
-import bunyan from "bunyan";
 import {Client} from "ldapjs";
 import {promisify} from "util";
-import {ICourse, User} from "./User";
+import {User} from "./User";
+
+import logger from "../Logger";
 
 const ldap = require('ldapjs');
-
-const logger = bunyan.createLogger({
-    name: 'ldap',
-    streams: [{
-        level: 'debug',
-        path: '/tmp/ldap.log',
-    }],
-});
 
 async function authenticate(uid: string, passwd: string): Promise<User> {
     const user = new User(uid);
     if (passwd === '') {
         return Promise.reject('no anonymous binds');
     }
+
     const client = ldap.createClient({url: process.env.LDAP_URL, log: logger});
     const rdn = `uid=${uid},ou=people,dc=bc,dc=edu`;
     const promisifiedBind = promisify(client.bind).bind(client);
@@ -27,37 +21,27 @@ async function authenticate(uid: string, passwd: string): Promise<User> {
         return Promise.reject('bind failed');
     }
     user.found();
-    getCourses(user, client);
+    await getCourses(user, client);
     return user;
 }
 
 async function getCourses(user: User, client: Client) {
-    const promisifiedSearch = promisify(client.search).bind(client);
+    await search(user, client);
+}
+
+async function search(user: User, client: Client) {
     const opts = {
         filter: `(uid=${user.uid})`,
         scope: 'sub'
     };
-    const searchResult = await promisifiedSearch('dc=bc,dc=edu', opts);
-
-
-    //const search_response = await promisifiedSearch('dc=bc,dc=edu', opts);
-}
-
-function compileResult(searchResult: any) {
-    const courses: string[] = [];
-
     return new Promise((resolve, reject) => {
-        searchResult.on('searchEntry', (entry: any) => {
-            const course: ICourse = {
-                courseNum: 'foo',
-                sectionNum: 'bar'
-            };
-            console.log(entry);
-        });
-
-        searchResult.on("end", (retVal: any) => {
-            console.log('end');
-            console.log(retVal);
+        client.search('dc=bc,dc=edu', opts, (error, res) => {
+            if (error) {
+                reject('LDAP search error');
+            }
+            res.on('searchEntry', entry => {
+                resolve(user);
+            });
         });
     });
 }
