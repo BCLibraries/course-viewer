6d219758dcc40baa4b4eb4c8024ab7c4083777a1
Benjamin Florin <benjamin.florin@gmail.com>
2018-08-10 13:03:34 -0400

    Block low-quality images

--- client/src/LibrarianBox.tsx
+++ client/src/LibrarianBox.tsx
@@ -2,7 +2,14 @@ import * as React from 'react';
 import Course from "./Course";
 import './LibrarianBox.css';
 
-class LibrarianBox extends React.Component<{ course: Course, loading: boolean }, {}> {
+class LibrarianBox extends React.Component<{ course: Course, loading: boolean }, { hiddenImgs: string[] }> {
+    public constructor(params: any) {
+        super(params);
+        this.state = {
+            hiddenImgs: []
+        }
+    }
+
     public render() {
         if (this.props.loading) {
             return (
@@ -30,11 +37,26 @@ class LibrarianBox extends React.Component<{ course: Course, loading: boolean },
         return (
             <div>
                 <h4>Your {libTerm}</h4>
+
+
                 {experts.map((expert: any, index: any) => {
+                        const imgKey = `${expert.firstName}${expert.lastName}`;
+                        const hiddenClass = this.state.hiddenImgs.indexOf(imgKey) > -1 ? 'hidden' : '';
+
+                        const filterSmallImages = ({target: img}: any) => {
+                            if (img.offsetWidth < 200) {
+                                this.setState(prevState => {
+                                    const hiddenImgs = prevState.hiddenImgs.slice(0);
+                                    hiddenImgs.push(imgKey);
+                                    return {...prevState, hiddenImgs};
+                                })
+                            }
+                        };
+
                         return (
                             <div key={index} className="librarian-box">
                                 <figure>
-                                    <img src={expert.imageUrl} alt='photo of subject librarian'/>
+                                    <img src={expert.imageUrl} className={hiddenClass} alt='photo of subject librarian' onLoad={filterSmallImages}/>
                                     <figcaption>
                                         <div className="name">
                                             <a href={expert.url} target="_blank">{expert.firstName} {expert.lastName}</a>
