67233419e486cd69ebc3be85032a450a649f9ace
Benjamin Florin <benjamin.florin@gmail.com>
2019-05-07 16:31:28 -0400

    Make research guides box functional.

--- client/src/ResearchGuidesBox.tsx
+++ client/src/ResearchGuidesBox.tsx
@@ -1,45 +1,55 @@
 import * as React from 'react';
 import Course from "./Course";
 import './ResearchGuidesBox.css';
+import ResearchGuidesPlaceholder from "./ResearchGuidesPlaceholder";
 
-class ResearchGuidesBox extends React.Component<{ course: Course, loading: boolean }, {}> {
-    public render() {
-        if (this.props.loading) {
-            return (
-                <div className="research-guides-block">
-                    <div className="ph-research-guides-header"/>
-                    <div className="ph-research-guides-header"/>
-                    <div className="ph-research-guides-header"/>
-                    <div className="ph-research-guides-header"/>
-                </div>
-            );
-        }
+/**
+ * Display relevant research guides
+ *
+ * @param loading
+ * @param course
+ * @constructor
+ */
+function ResearchGuidesBox({loading, course}: { course: Course, loading: boolean }) {
 
-        const course = this.props.course;
-
-        let subjectInfoLink = <span />
-        if (!course.researchGuides && (!course.subjectInfo || !course.subjectInfo.slug)) {
-            const url = `https://libguides.bc.edu/${course.subjectInfo.slug}`;
-            subjectInfoLink = <li><a href={url} target="_blank">{course.subjectInfo.name} resources</a></li>;
-        }
-
-        const guides = course.researchGuides.map(guideFactory);
-
-        return (
-            <div className="research-guides-block">
-                <h4>Research guides</h4>
-                <ul>
-                    {guides}
-                    {subjectInfoLink}
-                </ul>
-            </div>
-        );
+    // Show placeholder if we're still loading, otherwise...
+    if (loading) {
+        return <ResearchGuidesPlaceholder/>
     }
+
+    // Show the research guides.
+    return (
+        <div className="research-guides-block">
+            <h4>Research guides</h4>
+            <ul>
+                {course.researchGuides.map(guideFactory)}
+                {subjectInfoLink(course)}
+            </ul>
+        </div>
+    );
 }
 
+/**
+ * Build a research guide list item
+ *
+ * @param guide
+ */
 function guideFactory(guide: any) {
     const guideUrl = guide.friendlyUrl ? guide.friendlyUrl : guide.url;
-    return (<li key={guide.friendlyUrl}><a href={guideUrl} target="_blank">{guide.title}</a></li>);
+    return <li key={guide.friendlyUrl}><a href={guideUrl} target="_blank">{guide.title}</a></li>;
+}
+
+/**
+ * Link to the general subject guide if no relevant guides are found
+ *
+ * @param course
+ */
+function subjectInfoLink(course: Course) {
+    if (!course.researchGuides && (!course.subjectInfo || !course.subjectInfo.slug)) {
+        const url = `https://libguides.bc.edu/${course.subjectInfo.slug}`;
+        return <li><a href={url} target="_blank">{course.subjectInfo.name} resources</a></li>;
+    }
+    return <span/>;
 }
 
 export default ResearchGuidesBox;
---
+++ client/src/ResearchGuidesPlaceholder.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+
+/**
+ * Placeholder for research guides during course load
+ *
+ * @constructor
+ */
+function ResearchGuidesPlaceholder() {
+    return (
+        <div className="research-guides-block">
+            <div className="ph-research-guides-header"/>
+            <div className="ph-research-guides-header"/>
+            <div className="ph-research-guides-header"/>
+            <div className="ph-research-guides-header"/>
+        </div>
+    );
+}
+
+export default ResearchGuidesPlaceholder;
