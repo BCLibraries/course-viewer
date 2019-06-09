e1e94a182fb7d484ce38a14a8c945e1f2ab61674
Benjamin Florin <benjamin.florin@gmail.com>
2019-06-09 15:06:16 -0400

    Fix broken searchable ID code

--- server/src/courses/CourseSearcher.ts
+++ server/src/courses/CourseSearcher.ts
@@ -30,7 +30,7 @@ async function searchForCourse(code: string, section: string): Promise<Course> {
     }
 
     // If that fails, use just the course code as an identifier.
-    if (!courseFromAlma && code.match(/[Xx]\d/)) {
+    if (!courseFromAlma) {
         courseFromAlma = await findActiveCourse(`searchable_ids~${code}`);
     }
 
