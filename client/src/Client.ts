2d8b0edd1b86489c2e95fa18f0fe7313fdf7976e
Benjamin Florin <benjamin.florin@gmail.com>
2019-12-05 12:17:41 -0500

    Extract course number and section from API queries

--- client/src/Client.ts
+++ client/src/Client.ts
@@ -7,7 +7,11 @@ import Course from "./Course";
  */
 function buildCourse(courseJson: any): Course{
     const course = new Course();
+    const match = courseJson.code.match(/([A-Z]{4,6})(\d+X?)/);
+
     course.code = courseJson.code;
+    course.number = match ? match[0] : '';
+    course.section = courseJson.section;
     course.name = courseJson.name;
     course.hasReadings = courseJson.has_readings;
     course.subjectInfo = courseJson.subject_info;
@@ -16,7 +20,9 @@ function buildCourse(courseJson: any): Course{
     // If the course has no readings, return an empty course readings list.
     course.lists = course.hasReadings ? courseJson.reading_lists : [{citations: []}];
 
+    // Course object are saved as state and should be immutable.
     Object.freeze(course);
+
     return course;
 }
 
