562090596fc2a2188c71011bee84694e12b16840
Benjamin Florin <benjamin.florin@gmail.com>
2019-12-09 14:53:50 -0500

    Simplify App into functionl component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import {useState} from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
 import './App.css';
 import UserStorage from './UserStorage';
@@ -8,54 +9,51 @@ import LogoutButton from "./LogoutButton";
 
 const alternateBase = '/reserves';
 
-class App extends React.Component<{}, { user: any }> {
-    public constructor(props: any) {
-        super(props);
+function App() {
+    const [user, setUser] = useState(seedUser());
+
+    function storeNewUser(newUser: any) {
         try {
-            this.state = {user: UserStorage.get()};
+            UserStorage.store(newUser);
         } catch (e) {
-            this.state = {user: null}
+            // no-op
         }
+        setUser(newUser);
     }
 
+    const redirect = UserStorage.getReturnUrl();
+    const homepageRender = (props: any) =>
+        <Homepage user={user} setUser={setUser} redirectUrl={redirect}/>;
+    const courseRender = (props: any) => <CourseDisplayContainer {...props} user={user}/>;
 
-    public render() {
-        const setUser = (user: any) => {
-            try {
-                UserStorage.store(user);
-            } catch (e) {
-                // no-op
-            }
-            this.setState({user})
-        };
+    return (
+        <BrowserRouter basename={process.env.REACT_APP_CLIENT_BASE}>
+            <div>
+                <LogoutButton user={user} setUser={storeNewUser}/>
+                <Route exact={true} path={`${alternateBase}/`} render={homepageRender}/>
+                <Route exact={true} path={`${process.env.PUBLIC_URL}/`} render={homepageRender}/>
 
-        const redirect = UserStorage.getReturnUrl();
+                <Route path={`${alternateBase}/:course_id/section/:section_id`} render={courseRender}/>
+                <Route path={`${process.env.PUBLIC_URL}/:course_id/section/:section_id`} render={courseRender}/>
 
-        const homepageRender = (props: any) =>
-            <Homepage user={this.state.user} setUser={setUser} redirectUrl={redirect}/>;
-        const courseRender = (props: any) => <CourseDisplayContainer {...props} user={this.state.user}/>;
+                <Route path={`${alternateBase}/course/:course_id/section/:section_id`} render={courseRender}/>
+                <Route path={`${process.env.PUBLIC_URL}/course/:course_id/section/:section_id`} render={courseRender}/>
 
-        return (
-            <BrowserRouter basename={process.env.REACT_APP_CLIENT_BASE}>
-                <div>
-                    <LogoutButton user={this.state.user} setUser={setUser}/>
-                    <Route exact={true} path={`${alternateBase}/`} render={homepageRender}/>
-                    <Route exact={true} path={`${process.env.PUBLIC_URL}/`} render={homepageRender}/>
+                <Route path={`${alternateBase}/lti`} render={courseRender}/>
+                <Route path={`${process.env.PUBLIC_URL}/lti`} render={courseRender}/>
 
-                    <Route path={`${alternateBase}/:course_id/section/:section_id`} render={courseRender}/>
-                    <Route path={`${process.env.PUBLIC_URL}/:course_id/section/:section_id`} render={courseRender}/>
+                <div className="col-md-12 report-a-problem">Problems? <a href="mailto:reserves-ggroup@bc.edu">Send
+                    us an email</a></div>
+            </div>
+        </BrowserRouter>
+    );
+}
 
-                    <Route path={`${alternateBase}/course/:course_id/section/:section_id`} render={courseRender}/>
-                    <Route path={`${process.env.PUBLIC_URL}/course/:course_id/section/:section_id`} render={courseRender}/>
-
-                    <Route path={`${alternateBase}/lti`} render={courseRender}/>
-                    <Route path={`${process.env.PUBLIC_URL}/lti`} render={courseRender}/>
-
-                    <div className="col-md-12 report-a-problem">Problems? <a href="mailto:reserves-ggroup@bc.edu">Send
-                        us an email</a></div>
-                </div>
-            </BrowserRouter>
-        );
+function seedUser() {
+    try {
+        return UserStorage.get();
+    } catch (e) {
+        return null;
     }
 }
 
