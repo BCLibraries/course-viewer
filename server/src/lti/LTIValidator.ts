68bab62532b4e8cf196f52f61ff7165f839f0e94
Benjamin Florin <benjamin.florin@gmail.com>
2019-12-13 14:47:32 -0500

    Better documentation for LTI code

--- server/src/lti/LTIValidator.ts
+++ server/src/lti/LTIValidator.ts
@@ -1,14 +1,20 @@
 import {Request} from "express";
 
+/**
+ * Make sure the request is a valid LTI request
+ *
+ * @param req
+ */
 function validate(req: Request): boolean {
     const body = req.body;
+
+    // Make sure all required fields are present.
     const reqFields: string[] = [
         'lti_message_type',
         'lti_version',
         'oauth_consumer_key',
         'resource_link_id'
     ];
-
     const reqFieldsPresent = reqFields.every((field: string): boolean => {
         return body[field];
     });
@@ -17,6 +23,7 @@ function validate(req: Request): boolean {
         return false;
     }
 
+    // Make sure it's an LTI launch with an appropriate version.
     if (body.lti_message_type !== 'basic-lti-launch-request' && body.lti_version !== 'LTI-1p0') {
         return false;
     }
--- server/src/lti/Router.ts
+++ server/src/lti/Router.ts
@@ -4,43 +4,66 @@ import logger from '../Logger';
 
 const router = require('express').Router();
 
+// Route to launch LTI
 router.post('/', launch);
 
-
-router.get('/config.xml', sendConfig);
-router.get('/config', sendConfig);
-
-function sendConfig(req: Request, res: Response) {
-    const launchUrl = `https://${req.get('host')}${process.env.API_URL_ROOT}${req.baseUrl}`;
-    res.set('Content-Type', 'application/rss+xml');
-    res.render('config.xml.pug', {launchUrl});
-}
-
+/**
+ * Launch the LTI
+ *
+ * @param req
+ * @param res
+ */
 function launch(req: Request, res: Response) {
+
+    // Validate that we have received a proper LTI request.
     if (!validate(req)) {
         res.send({status: false});
     }
 
+    // Extract the course and section information from the LTI request.
     const regExp = /([A-Z]{4}\d{4})(X\w|\d\d)\d{4}[SFU]/;
     let courseId = '';
     let sectionId = 'X';
+
+    // If there is no sourcedid, use the context label. This shouldn't happen often.
     if (!req.body.lis_course_offering_sourcedid) {
         courseId = req.body.context_label;
-    } else if (req.body.lis_course_offering_sourcedid === 'LIBS503X') {
+    }
+
+    // Special case for test course
+    else if (req.body.lis_course_offering_sourcedid === 'LIBS503X') {
         courseId = 'LIBS503X';
         sectionId = '01';
-    } else {
+    }
+
+    // Majority of cases, parse the sourcedid to figure out the course and section numbers.
+    else {
         const match = req.body.lis_course_offering_sourcedid.match(regExp);
         if (match) {
             courseId = match[1];
             sectionId = match[2];
         } else {
+
+            // No match? The sourcedid is probably a whole searchable course ID.
             courseId = req.body.lis_course_offering_sourcedid;
         }
     }
+
+    // Redirect to the appropriate course display page.
     const url = `https://library.bc.edu/courses/${courseId}/section/${sectionId}`;
     logger.info({type: 'lti-launch-request', body: req.body, courseId: courseId, sectionId: sectionId, url: url});
     res.redirect(303, url);
+}
+
+// Routes for LTI configuration cartridge
+router.get('/config.xml', sendConfig);
+router.get('/config', sendConfig);
+
+// Send the XML configuration cartridge.
+function sendConfig(req: Request, res: Response) {
+    const launchUrl = `https://${req.get('host')}${process.env.API_URL_ROOT}${req.baseUrl}`;
+    res.set('Content-Type', 'application/rss+xml');
+    res.render('config.xml.pug', {launchUrl});
 }
 
 export default router;
