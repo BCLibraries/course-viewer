9f7bff94273afa20cb2764d8d222c388edaa8a74
Benjamin Florin <benjamin.florin@gmail.com>
2021-09-08 16:52:58 -0400

    Accommodate parsing different LDAP section IDs

--- server/src/auth/Section.ts
+++ server/src/auth/Section.ts
@@ -10,16 +10,31 @@ class Section {
     public readonly year: number;
 
     public constructor(ldapId: string) {
+
+        // Strip all hyphens to normalize between old-style (NURS2122-02-2021FALL) and new-style
+        // (NURS2122022021F) codes.
         this.ldapIdentifier = ldapId;
-        this.subjectCode = ldapId.substr(0, 4);
-        this.courseNum = ldapId.substr(4, 4);
-        this.sectionNum = ldapId.substr(9, 2);
-        this.year = +ldapId.substr(12, 4);
-        const semesterCode = ldapId.substr(16, 4);
-        this.semester = semesterCodeToLetter(semesterCode);
+        const normalizedLdapId = ldapId.replace(/-/g, '');
+
+        this.subjectCode = normalizedLdapId.substr(0, 4);
+        this.courseNum = normalizedLdapId.substr(4, 4);
+        this.sectionNum = normalizedLdapId.substr(8, 2);
+        this.year = +normalizedLdapId.substr(10, 4);
+
+        const restOfString = normalizedLdapId.substr(14)
+        this.semester = semesterCodeToLetter(restOfString);
     }
 }
 
+/**
+ * Convert end of LDAP section ID to semester code
+ *
+ * LDAP section IDs can end in a full semester string (e.g. NURS2122-02-2021FALL)
+ * or an abbreviated form (e.g. NURS2122022021F). This fuction takes whichever
+ * form and returns just a single-character code.
+ *
+ * @param {string} code
+ */
 function semesterCodeToLetter(code: string): string {
     switch (code) {
         case 'SUMM':
@@ -28,8 +43,11 @@ function semesterCodeToLetter(code: string): string {
         case 'FALL':
             return 'F';
             break;
+        case 'U':
+            return 'U';
+        case 'F':
+            return 'F'
         default:
-            console.log(code);
             return 'S';
     }
 }
